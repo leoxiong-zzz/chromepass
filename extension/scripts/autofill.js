be158c5b59591641ec121419d7698772e7c5f4e6
Leo Xiong <hello@leoxiong.com>
2016-07-25 02:26:17 +1200

    Fix/improve login form detection

--- extension/scripts/autofill.js
+++ extension/scripts/autofill.js
@@ -1,21 +1,28 @@
 (function () {
-    var pass = $('form input[type=password]:visible')[0];
-    var form = $(pass).closest('form');
-    var user = $(form).find('input[type=text], input[type=email]')[0];
+    NodeList.prototype.forEach = Array.prototype.forEach;
 
+    document.querySelectorAll('form input[type=password]').forEach(function (pass) {
+        var form = pass.closest('form');
+        var users = form.querySelectorAll('input[type=text], input[type=email]');
 
-    if ($(user) && $(pass)) {
-        chrome.runtime.sendMessage('', {
-            from: 'content_script',
-            action: 'fill_available'
-        });
-        chrome.runtime.onMessage.addListener(function (message, sender, respond) {
-            if (message.from == 'popup') {
-                if (message.action == 'do_fill') {
-                    $(user).val(message.user).css('background-color', '#edffe3');
-                    $(pass).val(message.pass).css('background-color', '#edffe3');
+        if (users.length == 1) {
+            var user = users[0];
+
+            chrome.runtime.onMessage.addListener(function (message, sender, respond) {
+                if (message.from == 'popup') {
+                    if (message.action == 'do_fill') {
+                        user.value = message.user;
+                        pass.value = message.pass;
+
+                        user.style.backgroundColor = '#edffe3';
+                        pass.style.backgroundColor = '#edffe3';
+                    }
                 }
-            }
-        });
-    }
+            });
+            chrome.runtime.sendMessage('', {
+                from: 'content_script',
+                action: 'fill_available'
+            });
+        }
+    });
 })();
