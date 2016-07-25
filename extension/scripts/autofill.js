da812960184a6ad6e15e2747a003099dea3fc3ee
Leo Xiong <hello@leoxiong.com>
2016-07-26 01:24:10 +1200

    Update autofill to pick up input without type text

--- extension/scripts/autofill.js
+++ extension/scripts/autofill.js
@@ -1,7 +1,12 @@
 (function () {
     Array.prototype.forEach.call(document.querySelectorAll('form input[type=password]'), function (pass) {
         var form = pass.closest('form');
-        var users = form.querySelectorAll('input[type=text], input[type=email]');
+
+        // <input> and <input type=""> do not get picked up by input[type=text] selector
+        // so we must select all inputs and filter it with the .type property
+        var users = Array.prototype.filter.call(form.querySelectorAll('input'), function (input) {
+            return input.type == 'text' || input.type == 'email';
+        });
 
         if (users.length == 1) {
             var user = users[0];
